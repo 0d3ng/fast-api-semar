1e0a59fd8ff5c54548fbbd7c3b8a630cdbfafe96
Noprianto <noprianto@polinema.ac.id>
2024-12-05 00:48:18 +0900

    ad device

--- app/schemas/device_schema.py
+++ app/schemas/device_schema.py
@@ -1,28 +1,38 @@
 #  """
 #  Copyright (c) 2024 lepen - All Rights Reserved
-#  Created by lepen on 2024-12-04 22:57:28
+#  Created by lepen on 2024-12-05 00:35:44
 #
 #  Author: lepen
 #  Email: noprianto@s.okayama-u.ac.jp
-#  Last modified: 2024-12-04 22:57:28
-#  File: device.py
+#  Last modified: 2024-12-05 00:35:44
+#  File: device_schema.py
 #  Description:
 #  """
 from typing import Optional
 
-from pydantic import BaseModel, Field, ConfigDict
+from pydantic import BaseModel, ConfigDict, Field
 
 from app.utils.custom_fields import PydanticObjectId
 
 
-class Device(BaseModel):
-    id: Optional[PydanticObjectId] = Field(alias='_id', default_factory=PydanticObjectId)
-    code: str
+class DeviceCreateUpdate(BaseModel):
     name: str
     type: str
     protocol: str
     project_id: str
     active: Optional[bool] = Field(default=True)
+
+    model_config = ConfigDict(arbitrary_types_allowed=True)
+
+
+class DeviceResponse(BaseModel):
+    id: PydanticObjectId = Field(alias='_id')
+    code: str
+    name: str
+    type: str
+    protocol: str
+    project_id: str
+    active: bool
     inserted_at: Optional[str] = None
     inserted_by: Optional[str] = None
     updated_at: Optional[str] = None
---
+++ app/services/device_service.py
@@ -0,0 +1,113 @@
+#  """
+#  Copyright (c) 2024 lepen - All Rights Reserved
+#  Created by lepen on 2024-12-05 00:41:25
+#
+#  Author: lepen
+#  Email: noprianto@s.okayama-u.ac.jp
+#  Last modified: 2024-12-05 00:41:25
+#  File: device_service.py
+#  Description:
+#  """
+import traceback
+from datetime import datetime
+
+from bson import ObjectId
+from fastapi import HTTPException
+from passlib.context import CryptContext
+from pytz import timezone
+
+from app.models.device import Device
+from app.schemas.device_schema import DeviceCreateUpdate, DeviceResponse
+from app.utils.db import db
+from app.utils.logger import get_logger
+
+logger = get_logger(__name__)
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+datetime_jpn = datetime.now(tz=timezone("Asia/Tokyo")).strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
+
+
+class DeviceService:
+    @staticmethod
+    async def create_device(device: DeviceCreateUpdate, current_user: str):
+        try:
+            logger.info(device)
+            new_device: Device = Device(
+                name=device.name,
+                description=device.description,
+                inserted_at=datetime_jpn,
+                inserted_by=current_user
+            )
+            logger.info(new_device)
+            new_device_inserted = await db.devices.insert_one(new_device.model_dump(by_alias=True))
+            new_user_id = new_device_inserted.inserted_id
+            return DeviceResponse(_id=new_user_id,
+                                   name=device.name,
+                                   description=device.description,
+                                   inserted_at=datetime_jpn,
+                                   inserted_by=current_user)
+        except Exception as e:
+            logger.error(f"Failed to create role: {e}")
+            tb_str = ''.join(traceback.format_tb(e.__traceback__))
+            logger.error(f"{e}\n{tb_str}")
+            raise HTTPException(status_code=500, detail=str(e))
+
+    @staticmethod
+    async def get_device(role_id: str):
+        try:
+            project = await db.devices.find_one({"_id": ObjectId(role_id)})
+            if project:
+                return DeviceResponse(**project)
+            raise HTTPException(status_code=404, detail="Role not found")
+        except (KeyError, TypeError, Exception) as e:
+            tb_str = "".join(traceback.format_tb(e.__traceback__))
+            logger.error(f"{e}\n{tb_str}")
+            raise HTTPException(status_code=500, detail=str(e))
+
+    @staticmethod
+    async def get_all_devices():
+        try:
+            projects = []
+            cursor = db.devices.find({})
+            async for project in cursor:
+                logger.info(f"{project} {project["_id"]}")
+                project_response = DeviceResponse(**project)
+                projects.append(project_response)
+                logger.info("")
+            return projects
+        except (KeyError, TypeError, Exception) as e:
+            tb_str = "".join(traceback.format_tb(e.__traceback__))
+            logger.error(f"{e}\n{tb_str}")
+            raise HTTPException(status_code=500, detail=str(e))
+
+    @staticmethod
+    async def update_device(device_id: str, device_update: DeviceCreateUpdate, current_user: str):
+        try:
+            update_data = {k: v for k, v in device_update.model_dump(exclude_unset=True).items() if v is not None}
+            update_data["updated_at"] = datetime_jpn
+            update_data["updated_by"] = current_user
+            logger.info(update_data)
+            result = await db.devices.update_one({"_id": ObjectId(device_id)}, {"$set": update_data})
+            logger.info(result)
+            if result.matched_count == 1:
+                return await DeviceService.get_device(device_id)
+            return None
+        except (KeyError, TypeError, Exception) as e:
+            tb_str = "".join(traceback.format_tb(e.__traceback__))
+            logger.error(f"{e}\n{tb_str}")
+            raise HTTPException(status_code=500, detail=str(e))
+
+    @staticmethod
+    async def delete_device(project_id: str, current_user: str):
+        try:
+            update_data = {
+                "deleted_at": datetime_jpn,
+                "deleted_by": current_user
+            }
+            result = await db.devices.update_one({"_id": ObjectId(project_id)}, {"$set": update_data})
+            if result.matched_count == 1:
+                return True
+            return False
+        except (KeyError, TypeError, Exception) as e:
+            tb_str = "".join(traceback.format_tb(e.__traceback__))
+            logger.error(f"{e}\n{tb_str}")
+            raise HTTPException(status_code=500, detail=str(e))

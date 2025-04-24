61620e7ec8d848e25f9a3f9c136ced6bdc747148
0d3ng <noprianto@polinema.ac.id>
2025-04-24 23:13:47 +0900

    add folder logs

--- app/utils/logger.py
+++ app/utils/logger.py
@@ -21,20 +21,19 @@ def get_logger(name):
 
     # Create handlers
     console_handler = logging.StreamHandler()
-    # file_handler = RotatingFileHandler(LOG_FILE, maxBytes=1000000, backupCount=3)
+    file_handler = RotatingFileHandler(LOG_FILE, maxBytes=5 * 1024 * 1024, backupCount=3)
 
     # Create formatters and add them to the handlers
     console_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s (%(filename)s:%(lineno)d)')
-    # file_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s (%(filename)s:%(lineno)d)')
+    file_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s (%(filename)s:%(lineno)d)')
     console_handler.setFormatter(console_format)
-    # file_handler.setFormatter(file_format)
+    file_handler.setFormatter(file_format)
 
     # Add handlers to the logger
     logger.addHandler(console_handler)
-    # logger.addHandler(file_handler)
+    logger.addHandler(file_handler)
 
     return logger
-
 
 # Example usage
 # logger = get_logger(__name__)

b25b3b0434db709cb4325d49245c1ea510ba1e4b
0d3ng <noprianto@polnema.ac.id>
2026-07-02 09:37:40 +0900

    remove / on controller

--- app/routes/k8s/kub_sensor_routes.py
+++ app/routes/k8s/kub_sensor_routes.py
@@ -20,7 +20,7 @@ logger = get_logger(__name__)
 router = APIRouter()
 
 
-@router.post("/kub_sensors/", response_model=SensorActuatorResponse)
+@router.post("/kub_sensors", response_model=SensorActuatorResponse)
 async def create_sensor(sensor_data: SensorActuatorCreate):
     try:
         return await SensorActuatorService.create_sensor_data_without_token(sensor_data)
